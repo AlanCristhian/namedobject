b4b61ea8af713ea25c4e775c2a7801cbb0d66168
AlanCristhian <alan.cristh@gmail.com>
2016-09-24 14:26:39 -0300

    update version

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
 
 setup(
-    name="named",
-    version="0.0.1",
-    py_modules=["named"],
+    name="name",
+    version="0.0.3",
+    py_modules=["name"],
 
     zip_safe=True,
     author="Alan Cristhian",
     author_email="alan.cristh@gmail.com",
-    description="Objects with the `__name__` attribute.",
+    description="A library with a base class that "\
+                "stores the assigned name of an object.",
     license="MIT",
     keywords="data structure",
-    url="https://github.com/AlanCristhian/named",
+    url="https://github.com/AlanCristhian/name",
 )
