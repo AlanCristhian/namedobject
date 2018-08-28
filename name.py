87f7a5554e49b3eddd11a8e6c8d4be613d8a0c0d
unknown <alan.cristh@gmail.com>
2018-08-28 02:30:15 -0300

    Add Type annotations

--- name.py
+++ name.py
@@ -6,14 +6,20 @@
 'a'
 """
 
+from __future__ import annotations
+
 import inspect
+
+from types import FrameType
+from typing import Generator, Iterator, Dict, Any, Optional
 
 
 __all__ = ["AutoName"]
 
 
 # Yields all locals variables in the higher (calling) frames
-def _get_outer_locals(frame):
+_FrameGenerator = Generator[Dict[str, Any], None, None]
+def _get_outer_locals(frame: FrameType) -> _FrameGenerator:
     while frame:
         yield frame.f_locals
         frame = frame.f_back
@@ -29,22 +35,22 @@ class AutoName:
     'a'
 
     """
-    def __init__(self, count=0):
+    def __init__(self, count: int = 0) -> None:
         assert isinstance(count, int), \
                "Expected 'int' object, got '%s'" % count.__class__.__name__
         assert count >= 0, "Expected positive 'int' number, got '%r'" % count
         self.__count = count
-        self.__name = None
+        self.__name: Optional[str] = None
 
     # I define the '__iter__' method to give compatibility
     # with the unpack sequence assignment syntax.
-    def __iter__(self):
+    def __iter__(self) -> Iterator[AutoName]:
         # NOTE: I call 'type(self)' to warranty that this
         # method works even in a subclass of this.
         return (type(self)() for _ in range(self.__count))
 
     # Find the assigned name of the current object.
-    def _find_name(self, frame):
+    def _find_name(self, frame: FrameType) -> str:
         scope = _get_outer_locals(frame)
         # NOTE: The same object could have many names in differents scopes.
         # So, I stores all names in the 'scopes' var. The valid name is one
@@ -72,7 +78,7 @@ class AutoName:
         raise NameError("Can not find the name of this object.")
 
     @property
-    def __assigned_name__(self):
+    def __assigned_name__(self) -> str:
         """Find the name of the instance of the current class."""
         if self.__name is None:
             frame = inspect.currentframe().f_back
