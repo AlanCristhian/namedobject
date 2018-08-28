4e34aeb47b1bb88ab00c18e25370f0663bda1942
unknown <alan.cristh@gmail.com>
2018-08-28 02:29:10 -0300

    Add Type annotations

--- test_name.py
+++ test_name.py
@@ -1,18 +1,21 @@
+from __future__ import annotations
+
+from typing import no_type_check
 import unittest
 
 import name
 
 
 class AutoNameSuite(unittest.TestCase):
-    def test_object_instance(self):
+    def test_object_instance(self) -> None:
         obj = name.AutoName()
         self.assertTrue(isinstance(obj, name.AutoName))
 
-    def test_single_assignment(self):
+    def test_single_assignment(self) -> None:
         obj = name.AutoName()
         self.assertEqual(obj.__assigned_name__, "obj")
 
-    def test_NameError_in_multiple_assignment(self):
+    def test_NameError_in_multiple_assignment(self) -> None:
         description = "Can not assign a unique name to multiple variables\."
         with self.assertRaisesRegex(NameError, description):
             a = b = name.AutoName()
@@ -21,35 +24,36 @@ class AutoNameSuite(unittest.TestCase):
             a = b = name.AutoName()
             b.__assigned_name__
 
-    def test_unpacking(self):
+    def test_unpacking(self) -> None:
         x, y = name.AutoName(2)
         self.assertEqual(x.__assigned_name__, "x")
         self.assertEqual(y.__assigned_name__, "y")
 
-    def test_count_argument_type(self):
+    @no_type_check
+    def test_count_argument_type(self) -> None:
         description = "Expected 'int' object, got 'float'"
         with self.assertRaisesRegex(AssertionError, description):
             x, y = name.AutoName(3.5)
 
-    def test_count_sign(self):
+    def test_count_sign(self) -> None:
         description = "Expected positive 'int' number, got '-2'"
         with self.assertRaisesRegex(AssertionError, description):
             x, y = name.AutoName(-2)
 
-    def test_subclass(self):
+    def test_subclass(self) -> None:
         class SubClass(name.AutoName):
-            def __init__(self, count=0):
+            def __init__(self, count: int = 0) -> None:
                 super().__init__(count)
         obj = SubClass()
         self.assertEqual(obj.__assigned_name__, "obj")
 
-    def test_multiple_inheritance(self):
+    def test_multiple_inheritance(self) -> None:
         class Numeric:
-            def __init__(self, type):
+            def __init__(self, type: object) -> None:
                 self.__type__ = type
 
         class Symbol(Numeric, name.AutoName):
-            def __init__(self, type, count=0):
+            def __init__(self, type: object, count: int = 0) -> None:
                 Numeric.__init__(self, type)
                 name.AutoName.__init__(self, count)
 
@@ -57,15 +61,15 @@ class AutoNameSuite(unittest.TestCase):
         self.assertEqual(x.__assigned_name__, "x")
         self.assertEqual(x.__type__, complex)
 
-    def test_assigned_name_in_a_method_of_the_child_class(self):
+    def test_assigned_name_in_a_method_of_the_child_class(self) -> None:
         """ Test the stored name in a method of a class that inherit from
         `name.AutoName`.
         """
         class Atom(name.AutoName):
-            def __init__(self, count=0):
+            def __init__(self, count: int = 0) -> None:
                 super().__init__(count)
 
-            def __repr__(self):
+            def __repr__(self) -> str:
                 return self.__assigned_name__
 
         a, b, c = Atom(3)
